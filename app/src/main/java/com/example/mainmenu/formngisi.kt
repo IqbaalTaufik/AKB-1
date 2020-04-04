890c1d3b6ee01be7dcdf136f2169e8d57cd4e1ff
iqbaalt07 <pudge.warcraft3@gmail.com>
2020-04-04 17:46:10 +0700

    tugas pertama matkul AKB

---
+++ app/src/main/java/com/example/mainmenu/formngisi.kt
@@ -0,0 +1,12 @@
+package com.example.mainmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class formngisi : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formngisi)
+    }
+}
---
+++ app/src/main/java/com/example/mainmenu/keluaran.kt
@@ -0,0 +1,12 @@
+package com.example.mainmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class keluaran : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keluaran)
+    }
+}
