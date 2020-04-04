97d7e1c45dee6f2b7007feca4878960612dadfda
iqbaalt07 <pudge.warcraft3@gmail.com>
2020-04-04 19:14:32 +0700

    hai

--- app/src/main/java/com/example/mainmenu/keluaran.kt
+++ app/src/main/java/com/example/mainmenu/keluaran.kt
@@ -8,5 +8,6 @@ class keluaran : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keluaran)
+
     }
 }
