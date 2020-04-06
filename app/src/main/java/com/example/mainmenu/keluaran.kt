9d82b97c557c7a84c02b9dba1b2661cbdac46241
iqbaalt07 <pudge.warcraft3@gmail.com>
2020-04-06 23:35:23 +0700

    keenam

--- app/src/main/java/com/example/mainmenu/keluaran.kt
+++ app/src/main/java/com/example/mainmenu/keluaran.kt
@@ -1,12 +1,15 @@
 package com.example.mainmenu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.mainmenu.EXTRA_MESSAGE as EXTRA
 class keluaran : AppCompatActivity() {
 
     private lateinit var texte : TextView
+    private lateinit var btn10 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keluaran)
@@ -14,6 +17,7 @@ class keluaran : AppCompatActivity() {
         val nama = intent.getStringExtra(EXTRA)
         val kluar = "Beres! Sekarang "+nama+" udah bisa ngecek penggunaan HP mu tiap hari buat bantu "+nama+" ngatur waktu :)"
         texte.setText(kluar)
+        btn10 = findViewById(R.id.tmbl5)
 
     }
 }
