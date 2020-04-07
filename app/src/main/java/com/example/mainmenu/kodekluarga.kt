27d5fb42b4b728b1dcaf343ad0663c11e3942d2c
iqbaalt07 <pudge.warcraft3@gmail.com>
2020-04-07 18:48:17 +0700

    kedelapan

--- app/src/main/java/com/example/mainmenu/kodekluarga.kt
+++ app/src/main/java/com/example/mainmenu/kodekluarga.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import kotlinx.android.synthetic.*
 
 class kodekluarga : AppCompatActivity() {
@@ -14,8 +15,18 @@ private lateinit var btn1 : Button
         setContentView(R.layout.activity_kodekluarga)
         btn1 = findViewById(R.id.tmbl3)
         btn2 = findViewById(R.id.tmbl2)
+        val mskode = findViewById<EditText>(R.id.mkode)
+        val kd = mskode.text.toString()
         btn1.setOnClickListener{
             startActivity(Intent(this,formngisi::class.java))
+            if(kd.isEmpty())
+            {
+                mskode.setError("masukkan Kode")
+            }
+            else if(kd.isNotEmpty())
+            {
+                mskode.setError("kode salah")
+            }
         btn2.setOnClickListener{
             startActivity(Intent(this,formngisi::class.java))
         }
